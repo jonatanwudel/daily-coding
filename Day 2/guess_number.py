d6a2170690094fd5d84ca0958d56751c17af3913
Jonathan <jonatan.wudel.dev@gmail.com>
2023-05-06 17:52:16 +0200

    Feat: create simple guess_number game

---
+++ Day 2/guess_number.py
@@ -0,0 +1,40 @@
+'''
+guess the number
+'''
+import random
+
+
+def main():
+    print('Welcome to \"Guess the number\" game.')
+    computer_number = random.randrange(0,100)
+    print('Computer chose random number between 0 and 100')
+    # print(computer_number)
+    game(computer_number)
+    play_again()
+
+
+def play_again():
+    play_again = int(input('Insert 0 to exit, Insert 1 to play again: '))
+    if play_again == 0: return print('Have a good day')
+    elif play_again == 1: return main()
+    else: return print('Invalid number')
+
+
+def game(computer_number):
+    while True:
+        user_guess = int(input("Your guess?: "))
+        if user_guess == computer_number:
+            print('Good job you got it')
+            break
+        else:
+            if user_guess > computer_number:
+                print('It\'s lower')
+                continue
+
+            elif user_guess < computer_number:
+                print('It\'s higher')
+                continue
+
+
+if __name__ == '__main__':
+    main()
