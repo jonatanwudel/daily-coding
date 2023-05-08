8a0921e970e56222537574ae2f1eb26bbb1bfd90
Jonathan <jonatan.wudel.dev@gmail.com>
2023-05-08 21:16:42 +0200

    Feat: create simple countdown timer with sound alarm

---
+++ Day 4/countdown_timer.py
@@ -0,0 +1,21 @@
+'''
+countdown Timer
+'''
+import time
+from playsound import playsound
+#pip3 install playsound
+
+
+def countdown(user_time):
+    while user_time >= 0:
+        minutes, seconds = divmod(user_time, 60)
+        timer = '{:02d}:{:02d}'.format(minutes, seconds)
+        print(timer)
+        time.sleep(1)
+        user_time -= 1
+    print('End !!')
+    playsound('alarm.mp3')
+
+if __name__ == '__main__':
+    user_time = int(input('Enter time in seconds: '))
+    countdown(user_time)
