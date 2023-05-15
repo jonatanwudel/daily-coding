3ec11ddcec095c97018925878bcde8d6af1a91d5
Jonathan <jonatan.wudel.dev@gmail.com>
2023-05-15 21:56:53 +0200

    Style: remove scanner, add swing module and do simple gui

--- Day 7/src/GuessingGame.java
+++ Day 7/src/GuessingGame.java
@@ -1,24 +1,21 @@
 import java.util.Random;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class GuessingGame {
 
 	public static void main(String[] args) {
 		Random random = new Random();
-		Scanner scanner = new Scanner(System.in);
 		int userAnswer = 0;
 		int computerNumber = random.nextInt(100) + 1;
 		int count = 1;
 		System.out.println("Devloper mode, compouter number is: " + computerNumber);
 		
 		while (userAnswer != computerNumber) {
-			System.out.println("Guess number between 1 and 100");
-			String response = scanner.next();
+			String response = JOptionPane.showInputDialog(null, "Guess number between 1 and 100");
 			userAnswer = Integer.parseInt(response);
-			System.out.println(checkGuess(userAnswer, computerNumber, count));
+			JOptionPane.showMessageDialog(null, checkGuess(userAnswer, computerNumber, count));
 			count++;
 			
 		}
-		scanner.close();
 	}
 	
 	public static String checkGuess(int userAnswer, int computerNumber, int count) {
