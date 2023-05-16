5e603f0e310a3af27fd2e3b878271590d0da78de
Jonathan <jonatan.wudel.dev@gmail.com>
2023-05-16 19:28:25 +0200

    Feat: create dice roll emulator

--- Day 8/src/diceRollEmulator/diceRollEmulator.java
+++ Day 8/src/diceRollEmulator/diceRollEmulator.java
@@ -1,8 +1,29 @@
 package diceRollEmulator;
+import java.util.Scanner;
+import java.util.Random;
 
 public class diceRollEmulator {
 	public static void main(String[] args) {
-		//System.out.println("");
+		
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		System.out.println("Enter number of dice: ");
+		int diceNumber = scanner.nextInt();
+		
+		System.out.println("You rolled: ");
+		int totalNumber = 0;
+		int randomNumber = 0;
+		
+		for (int i = 0; i < diceNumber; i++) {
+			randomNumber = random.nextInt(6) + 1;
+			totalNumber = totalNumber + randomNumber;
+			System.out.println(randomNumber);
+		}
+		
+		System.out.println("Total: " + totalNumber);
+		scanner.close();
+		
 	}
 
 }
